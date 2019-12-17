1fd5a5b916d372628896e9f92710c35b5c86800c
Maly <migsking@naver.com>
2019-12-17 20:44:47 +0900

    [ADD] <app-register_views> create the view register func

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,18 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 
+def register_views(app: Flask):
+    from app.views.apis import createRoom
+    app.register_blueprint(createRoom.api.blueprint)
+
+
 def create_app(*config_cls):
     flask = Flask(__name__)
 
     for config in config_cls:
         flask.config.from_object(config)
+
+    register_views(flask)
 
     socketIO = SocketIO(flask)
 
---
+++ app/views/apis/createRoom.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+from flask_restful import Api, Resource
+
+
+api = Api(Blueprint(__name__, __name__))
+
+@api.resource('/room')
+class roomManagement(Resource):
+    def get(self):
+        return 'Hello'
