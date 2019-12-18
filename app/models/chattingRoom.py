6850bafd2c95e3066143243e9b81f703ffdd31d4
Maly <migsking@naver.com>
2019-12-18 19:44:44 +0900

    [ADD\ <models/chattingRoom.py> Create the chattingRoom Model

---
+++ app/models/chattingRoom.py
@@ -0,0 +1,25 @@
+from mongoengine import (
+    Document,
+    EmbeddedDocument,
+    EmbeddedDocumentField,
+    StringField,
+    ListField
+)
+
+
+class chatDataModel(EmbeddedDocument):
+    userId = StringField()
+
+    name = StringField()
+
+    chat = StringField()
+
+
+class chattingRoomModel(Document):
+    roomId = StringField(primary_key=True)
+
+    people = ListField()
+
+    chatData = ListField(
+        EmbeddedDocumentField(chatDataModel)
+    )
